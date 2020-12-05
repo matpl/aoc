848a2e98fbecafc28e84d8a3391fd2e2090c892c
Mathieu Plourde <mat.plourde@gmail.com>
2020-12-05 12:12:10 -0500

    use bitwise operations

--- 5/script.py
+++ 5/script.py
@@ -1,25 +1,17 @@
 file = open('input.txt','r')
 lines = list(map(lambda line : list(line.strip()), file.readlines()))
-
-ROW_RANGE = list(range(0,128))
-COL_RANGE = list(range(0,8))
-
 seats = []
 
 for line in lines:
-    row = ROW_RANGE[:]
-    col = COL_RANGE[:]
+    row = 127
+    col = 7
     for i in range(0,7):
         if line[i] == 'F':
-            row = row[:int(len(row)/2)]
-        else:
-            row = row[int(len(row)/2):]
+            row &= ~(64 >> i)
     for i in range(7,10):
         if line[i] == 'L':
-            col = col[:int(len(col)/2)]
-        else:
-            col = col[int(len(col)/2):]
-    seats.append(row[0] * 8 + col[0])
+            col &= ~(4 >> (i - 7))
+    seats.append(row * 8 + col)
 
 seats.sort()
 for i in range(0, len(seats) - 1):
