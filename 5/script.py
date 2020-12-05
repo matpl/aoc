c0362560488a8e36a022016a455801110a6da7f1
Mathieu Plourde <mat.plourde@gmail.com>
2020-12-05 15:59:43 -0500

    use 1 variable instead of 2

--- 5/script.py
+++ 5/script.py
@@ -3,18 +3,14 @@ lines = list(map(lambda line : list(line.strip()), file.readlines()))
 seats = []
 
 for line in lines:
-    row = 127
-    col = 7
-    for i in range(0,7):
-        if line[i] == 'F':
-            row &= ~(64 >> i)
-    for i in range(7,10):
-        if line[i] == 'L':
-            col &= ~(4 >> (i - 7))
-    seats.append(row * 8 + col)
+    pos = 1023
+    for i in range(0,10):
+        if line[i] == 'F' or line[i] == 'L':
+            pos &= ~(512 >> i)
+    seats.append((pos >> 3 << 3) + (pos & 7))
 
 seats.sort()
 for i in range(0, len(seats) - 1):
     if seats[i] + 1 != seats[i + 1]:
-        print(seats[i] + 1)
+        print(seats[i] + 1) # 676
         break
