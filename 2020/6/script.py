f0bf67723e65136578191ca8462bca9f1b304e5f
Mathieu Plourde <mat.plourde@gmail.com>
2020-12-06 00:33:04 -0500

    day 6 2020

---
+++ 2020/6/script.py
@@ -0,0 +1,17 @@
+file = open('input.txt','r')
+groups = list(map(lambda line : line, file.read().split('\n\n')))
+count = 0
+for group in groups:
+    people = group.split('\n')
+    questions = (2 << 25) - 1
+    for p in people:
+        p = list(map(lambda c: pow(2,ord(c) - 97), p))
+        bin = 0
+        for c in p:
+            bin |= c
+        questions &= bin
+
+    for i in range(0, 26):
+        count += (questions >> i) & 1
+
+print(count)
