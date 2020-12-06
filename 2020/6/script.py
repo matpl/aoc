8a75e1ad7b4a3bd2d8a0b24978037c410199034e
Mathieu Plourde <mat.plourde@gmail.com>
2020-12-06 00:40:38 -0500

    both day 6 solutions

--- 2020/6/script.py
+++ 2020/6/script.py
@@ -3,12 +3,14 @@ groups = list(map(lambda line : line, file.read().split('\n\n')))
 count = 0
 for group in groups:
     people = group.split('\n')
+    #questions = 0
     questions = (1 << 26) - 1
     for p in people:
         p = list(map(lambda c: 1 << (ord(c) - 97), p))
         bin = 0
         for c in p:
             bin |= c
+        #questions |= bin
         questions &= bin
 
     for i in range(0, 26):
